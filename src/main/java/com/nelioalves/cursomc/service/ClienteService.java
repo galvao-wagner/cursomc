18c5956068b264be127c3376dd359846dac51bd2
galvao-wagner <galvao.wagner@outlook.com.br>
2018-01-24 12:01:09 -0200

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/nelioalves/cursomc/service/ClienteService.java
@@ -0,0 +1,23 @@
+package com.nelioalves.cursomc.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nelioalves.cursomc.domain.Cliente;
+import com.nelioalves.cursomc.repository.ClienteRepository;
+import com.nelioalves.cursomc.service.exception.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {
+		Cliente obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Não encontrado Id: "+ id
+					+ " em " + Cliente.class.getName());
+		}
+		return obj;
+	}
+}
